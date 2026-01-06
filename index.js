e04deb4f03e4b85557d013d6b0997ad2b57138fe
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-01-06 18:05:42 +0000

    Quick Fix

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ function markAsDone(todos) {
     //return transform(0, todos, []);
     todos[count] = 'done - ' + todos[count];
     count++;
-}
+
     if(count < todos.length){
 
         return markAsDone(todos);
     }else{
         return todos;
     }
-
+}
 document.getElementById('result').innerHTML = markAsDone('one', 'two', 'three');
