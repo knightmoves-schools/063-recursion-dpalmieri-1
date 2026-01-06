85f0da0ed23108884d839a24fe68fdd09831b915
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-01-06 18:08:20 +0000

    Index.Javascript is quite a recursive problem for me

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ let count = 0;
 
 function markAsDone(todos) {
     //return transform(0, todos, []);
-    todos[count] = 'done - ' + todos[count];
     count++;
-
+    
     if(count < todos.length){
-
+    todos[count] = 'done - ' + todos[count];
         return markAsDone(todos);
     }else{
         return todos;
